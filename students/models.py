63abbdd24a6dcbcf8de7bee08379416acd618b1a
unniznd <aanandunnis@gmail.com>
2023-06-03 09:51:27 +0530

    migration added

--- students/models.py
+++ students/models.py
@@ -5,6 +5,7 @@ from branch_class.models import ClassDivisionModel
 class Students(models.Model):
     admission_number = models.IntegerField(unique=True, primary_key=True)
     student_name = models.CharField(max_length=200)
+    roll_number = models.IntegerField(default=0)
     student_branch = models.ForeignKey(ClassDivisionModel, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=10)
     is_absent = models.BooleanField(default=False)
