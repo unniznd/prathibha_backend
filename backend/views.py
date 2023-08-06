f128c608c2ec3d76ce5e6b8744382b728aca5196
unniznd <aanandunnis@gmail.com>
2023-08-06 21:49:38 +0530

    report fixed

--- backend/views.py
+++ backend/views.py
@@ -10,8 +10,11 @@ from users.permissions import IsAdminUserOrBranchAdminUser
 from attendance.models import Attendace   
 from attendance.serializers import StudentAttendanceReportSerializer
 from students.models import Students
+from fee.models import Fees
+
 from datetime import timedelta
 from datetime import datetime
+
 
 import csv
 
@@ -88,12 +91,13 @@ class AttendanceReport(ListAPIView):
 
 
 class FeeReport(ListAPIView):
-    serializer_class = StudentAttendanceReportSerializer
     permission_classes = [IsAdminUserOrBranchAdminUser, IsAuthenticated]
         
 
     def get(self, request, branchId, *args, **kwargs):
-        queryset = Students.objects.all()
+        # get from month, to month and view as parameters from query params
+        # filter fee objects based on the above params
+
         from_date = datetime.strptime(self.request.query_params.get('from', None),'%Y-%m-%d')
         to_date = datetime.strptime(self.request.query_params.get('to', None),'%Y-%m-%d')
         view = self.request.query_params.get('view', None)
@@ -102,12 +106,12 @@ class FeeReport(ListAPIView):
             return Response({
                 "message": "Please provide from and to date"
             }, status=status.HTTP_400_BAD_REQUEST)
-
+        
         if from_date>to_date:
             return Response({
                 "message": "From date cannot be greater than to date"
             }, status=status.HTTP_400_BAD_REQUEST)
-
+        
         if view == "csv" :
             response = HttpResponse(content_type='text/csv')
             response['Content-Disposition'] = f'attachment; filename="fee_reports_{from_date} - {to_date}.csv"'
@@ -116,45 +120,39 @@ class FeeReport(ListAPIView):
             writer = csv.writer(response)
 
             # Write the header row
-            writer.writerow(['Admission Number', 'Student Name', 'Standard', 'Division', 'Date', 'Status'])
+            writer.writerow(['Admission Number', 'Student Name', 'Standard', 
+                             'Division', 'Date', 'Status', 'Installment'])
 
-          
-            while from_date<=to_date:
-                for student in queryset:
-                    writer.writerow([
-                        student.admission_number,
-                        student.student_name,
-                        student.student_branch.standard,
-                        student.student_branch.division,
-                        datetime.strftime(from_date, "%B %d, %Y"),
-                        "Paid" if student.fee_set.filter(date=from_date).first() else "Not Paid"
-                    ])
-                   
+            queryset = Fees.objects.filter(date_of_payment__gte=from_date, date_of_payment__lte=to_date)
 
-                from_date = from_date + timedelta(days=1)
+            for fee in queryset:
+                writer.writerow([
+                    fee.student.admission_number,
+                    fee.student.student_name,
+                    fee.student.student_branch.standard,
+                    fee.student.student_branch.division,
+                    datetime.strftime(fee.date_of_payment, "%B %d, %Y"),
+                    fee.status,
+                    fee.installment.installment
+                ])
 
             return response
-
+        
+        queryset = Fees.objects.filter(date_of_payment__gte=from_date, date_of_payment__lte=to_date)
         reports = []
-    
 
-        while from_date<=to_date:
-            for student in queryset:
-                report = {}
-                report["admission_number"] = student.admission_number
-                report["student_name"] = student.student_name
-                report["standard"] = student.student_branch.standard
-                report["division"] = student.student_branch.division
-                report["date"] = datetime.strftime(from_date, "%B %d, %Y")
-                report["status"] = "Paid"
-                fee = student.fee_set.filter(date=from_date).first()
-                if not fee:
-                    report["status"] = "Not Paid"
-                
-                reports.append(report)
-            
-        
-            from_date = from_date + timedelta(days=1)
+        for fee in queryset:
+            report = {}
+            report["admission_number"] = fee.student.admission_number
+            report["student_name"] = fee.student.student_name
+            report["standard"] = fee.student.student_branch.standard
+            report["division"] = fee.student.student_branch.division
+            report["date"] = datetime.strftime(fee.date_of_payment, "%B %d, %Y")
+            report["status"] = fee.status.capitalize()
+            report["installment"] = fee.installment.installment
+            reports.append(report)
 
-        
-        return Response({"status":True, "data":reports}, status=status.HTTP_200_OK)
+        return Response({
+            "status":True,
+            "data":reports
+        }, status=status.HTTP_200_OK)
